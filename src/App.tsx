549759e5ad97b8589bbfedf726425c92d0e8ca66
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-02 15:19:21 -0300

    feat: add SignUp page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn/Index';
+import SignUp from './pages/SignUp/Index';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
