99be44c974bfaaaa40803355d038d74a71d3b082
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-11-03 05:46:12 -0300

    feat: logout of application

--- src/pages/SignIn/Index.tsx
+++ src/pages/SignIn/Index.tsx
@@ -9,7 +9,7 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 import { Container, Content, Background } from './styles';
 import getValidationErrors from '../../utils/getValidationErrors';
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 
 interface SignInFormData {
   email: string;
